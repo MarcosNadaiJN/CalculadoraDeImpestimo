65138e0cd691c0b52d9de4658936315144ef51c2
mysterynd15 <marcosnadaijn@gmail.com>
2020-10-23 08:59:09 -0300

    Tela Atualizada

---
+++ MAPA-PDS2-BancoWZT/src/mapa/pds2/bancowzt/MAPAPDS2BancoWZT.java
@@ -0,0 +1,10 @@
+package mapa.pds2.bancowzt;
+
+public class MAPAPDS2BancoWZT {
+    
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+        tela.setVisible(true);
+    }
+    
+}
